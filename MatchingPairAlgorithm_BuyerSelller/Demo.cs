b6894306fea50dea48aee5c692682ee53cdd0bfc
malavshah9 <mdshah9574@gmail.com>
2020-01-22 17:06:02 +0530

    16 conditions added

--- MatchingPairAlgorithm_BuyerSelller/Demo.cs
+++ MatchingPairAlgorithm_BuyerSelller/Demo.cs
@@ -7,204 +7,233 @@ using System.Threading.Tasks;
 
 namespace MatchingPairAlgorithm_BuyerSelller
 {
-    public class party
+    public class NewParty
     {
         public int qty;
         public int a;
         public int range;
-        public party(int q,int allOrNone,int r)
+        public NewParty(int q,int allOrNone,int r)
         {
             this.qty = q;
             this.a = allOrNone;
             this.range = r;
         }
+        public String toString()
+        {
+            return $"Quantity is {qty} , AllOrNone{a} and {range}";
+        }
     }
     class Demo
     {
-        List<party> buyers = new List<party>();
-        List<party> sellers = new List<party>();
-        public void simplyUpdate(int buyerIndex,int sellerIndex)
+        List<NewParty> buyers = new List<NewParty>();
+        List<NewParty> sellers = new List<NewParty>();
+        public void updateQty(NewParty partyA,NewParty partyB,int trade)
         {
-            int flag = 0;
-            if(this.buyers[buyerIndex].qty==this.sellers[sellerIndex].qty)
+            partyA.qty -= trade;
+            partyB.qty -= trade;
+        }
+        
+        public int isCompatibleGivesTrade(NewParty party1,NewParty party2)
+        {
+            int newQty = -1;
+            // Checks the condition of both party
+            // Returns the new quantity from party iff they are compatible
+            if (party1.qty == 0 || party2.qty == 0)
+                return newQty;
+            if(party1.a==0 && party2.a==0 && party1.range==0 && party2.range == 0)
             {
-                Console.WriteLine("Tradded Between Buyer" + buyers[buyerIndex].qty + " & seller" + sellers[sellerIndex].qty);
-                buyers.RemoveAt(buyerIndex);
-                sellers.RemoveAt(sellerIndex);
+                newQty=Math.Min(party1.qty, party2.qty);
             }
-            else if(this.buyers[buyerIndex].qty > this.sellers[sellerIndex].qty)
+            else if(party1.a == 0 && party1.range == 0 && party2.a != 0 && party2.range == 0)
             {
-                Console.WriteLine("Tradded Between Buyer" + buyers[buyerIndex].qty + " & seller" + sellers[sellerIndex].qty);
-                buyers[buyerIndex].qty -= sellers[sellerIndex].qty;
-                sellers.RemoveAt(sellerIndex);
+                if (party1.qty >= party2.qty)
+                {
+                    newQty=party2.qty;
+                }
+            }
+            else if (party1.a != 0 && party1.range == 0 && party2.a == 0 && party2.range == 0)
+            {
+                if (party2.qty >= party1.qty)
+                {
+                    newQty = party1.qty;
+                }
+            }
+            else if(party1.a != 0 && party1.range == 0 && party2.a != 0 && party2.range == 0)
+            {
+                if (party1.qty == party2.qty)
+                {
+                    newQty = party1.qty;
+                }
+            }
+            else if (party1.a == 0 && party1.range != 0 && party2.a == 0 && party2.range != 0)
+            {
+                if (party1.qty == party2.qty)
+                {
+                    newQty = party1.qty;
+                }
+                else if(party1.range<=party2.range && party1.qty > party2.qty)
+                {
+                    newQty= party2.qty;
+                }
+                else if(party2.range<=party1.range && party2.qty > party1.qty)
+                {
+                    newQty= party1.qty;
+                }
+                else if (party1.range<=party2.range && party2.qty>party1.qty)
+                {
+                    newQty= party1.qty;
+                }
+                else if (party2.range <= party1.range && party1.qty > party2.qty)
+                {
+                    newQty= party2.qty;
+                }
+                else if (party1.qty<party2.qty)
+                {
+                    newQty= party1.qty;
+                }
+                else
+                {
+                    newQty= party2.qty;
+                }
+            }
+            else if (party1.a == 0 && party1.range == 0 && party2.a == 0 && party2.range != 0)
+            {
+               if(party1.qty>=party2.range && party1.qty <= party2.qty)
+                {
+                    newQty=party1.qty;
+                }
+               else if (party1.qty >= party2.qty)
+                {
+                    newQty=party2.qty;
+                }
+            }
+            else if (party2.a == 0 && party2.range == 0 && party1.a == 0 && party1.range != 0)
+            {
+                if (party2.qty >= party1.range && party2.qty <= party1.qty)
+                {
+                    newQty = party2.qty;
+                }
+                else if (party2.qty >= party1.qty)
+                {
+                    newQty = party1.qty;
+                }
+            }
+            else if(party1.a != 0 && party1.range == 0 && party2.a == 0 && party2.range != 0)
+            {
+                if(party1.qty>=party2.range && party1.qty<=party2.qty)
+                {
+                    newQty = party1.qty;
+                }
+            }
+            else if (party2.a != 0 && party2.range == 0 && party1.a == 0 && party1.range != 0)
+            {
+                if (party2.qty >= party1.range && party2.qty <= party1.qty)
+                {
+                    newQty = party2.qty;
+                }
+            }
+            else if (party1.a != 0 && party1.range == 0 && party2.a != 0 && party2.range != 0)
+            {
+                if(party1.qty==party2.qty)
+                {
+                    newQty = party1.qty;
+                }
+            }
+            else if (party2.a != 0 && party2.range == 0 && party1.a != 0 && party1.range != 0)
+            {
+                if (party1.qty == party2.qty)
+                {
+                    newQty = party2.qty;
+                }
+            }
+            else if (party1.a == 0 && party1.range == 0 && party2.a != 0 && party2.range != 0)
+            {
+                if (party1.qty >= party2.qty)
+                    newQty = party2.qty;
+            }
+            else if (party2.a == 0 && party2.range == 0 && party1.a != 0 && party1.range != 0)
+            {
+                if (party2.qty >= party1.qty)
+                    newQty = party1.qty;
+            }
+            else if (party1.a != 0 && party1.range != 0 && party2.a == 0 && party2.range != 0)
+            {
+                if (party1.qty == party2.qty)
+                {
+                    newQty = party2.qty;
+                }
+                else if(party1.qty ==party2.range)
+                {
+                    newQty = party1.qty;
+                }
+            }
+            else if (party2.a != 0 && party2.range != 0 && party1.a == 0 && party1.range != 0)
+            {
+                if (party2.qty == party1.qty)
+                {
+                    newQty = party1.qty;
+                }
+                else if (party2.qty == party1.range)
+                {
+                    newQty = party2.qty;
+                }
             }
             else
             {
-                Console.WriteLine("Tradded Between Buyer" + buyers[buyerIndex].qty + " & seller" + sellers[sellerIndex].qty);
-                sellers[sellerIndex].qty -= buyers[buyerIndex].qty;
-                buyers.RemoveAt(buyerIndex);
+                if (party1.qty == party2.qty)
+                {
+                    newQty = party1.qty;
+                }
+            }
+            return newQty;
+        }
+        public void loopThrough(List<NewParty> source,List<NewParty> destination)
+        {
+            for (int i=0; i < source.Count; i++)
+            {
+                for(int j = 0; j < destination.Count; j++)
+                {
+                    int trade = isCompatibleGivesTrade(source[i], destination[j]);
+                    if (trade != -1)
+                    {
+                        Console.WriteLine($"Trade between {source[i].qty} and {destination[j].qty} with trade {trade}");
+                        updateQty(source[i], destination[j], trade);
+                    }
+                }
             }
         }
-        public int withOneALlOrNoneCondition(party withoutCondition,party withCondition,int indexWithOutCondition,int indexWithCondition)
+        public void clean(List<NewParty> list,int index)
         {
-            int flag = 0;
-            if(withCondition.qty< withoutCondition.qty)
+            if (index < list.Count)
             {
-                if(buyers[indexWithOutCondition]==withoutCondition)
+                if (list[index].qty == 0)
                 {
-                    Console.WriteLine("Tradded Between Buyer" + buyers[indexWithOutCondition].qty + " & seller" + sellers[indexWithCondition].qty);
-                    buyers[indexWithOutCondition].qty -= sellers[indexWithCondition].qty;
-                    sellers.RemoveAt(indexWithCondition);
+                    list.RemoveAt(index);
+                    clean(list, index);
                 }
                 else
                 {
-                    Console.WriteLine("Tradded Between Buyer" + buyers[indexWithCondition].qty + " & seller" + sellers[indexWithOutCondition].qty);
-                    sellers[indexWithOutCondition].qty -= buyers[indexWithCondition].qty;
-                    buyers.RemoveAt(indexWithCondition);
+                    clean(list, index + 1);
                 }
             }
-            else if(withCondition.qty==withoutCondition.qty)
-            {
-                if (buyers[indexWithOutCondition] == withoutCondition)
-                {
-                    Console.WriteLine("Tradded Between Buyer" + buyers[indexWithOutCondition].qty + " & seller" + sellers[indexWithCondition].qty);
-                    buyers.RemoveAt(indexWithOutCondition);
-                    sellers.RemoveAt(indexWithCondition);
-                }
-                else
-                {
-                    Console.WriteLine("Tradded Between Buyer" + buyers[indexWithCondition].qty + " & seller" + sellers[indexWithOutCondition].qty);
-                    sellers.RemoveAt(indexWithOutCondition);
-                    buyers.RemoveAt(indexWithCondition);
-                }
-            }
-            else
-            {
-                flag = -1;
-            }
-            return flag;
-        }
-        public int withOneRangeCondition(party withOutRange, party withRange,int indexWithoutRange, int indexWithRange)
-        {
-            int flag = 0;
-            if(withOutRange.qty>=withRange.range)
-            {
-                if(withOutRange.qty>withRange.qty)
-                {
-                    if(buyers[indexWithoutRange]==withOutRange)
-                    {
-                        Console.WriteLine("Tradded Between Buyer" + buyers[indexWithoutRange].qty + " & seller" + sellers[indexWithRange].qty);
-                        buyers[indexWithoutRange].qty -= sellers[indexWithRange].qty;
-                        sellers.RemoveAt(indexWithRange);
-                    }
-                    else
-                    {
-                        Console.WriteLine("Tradded Between Buyer" + buyers[indexWithRange].qty + " & seller" + sellers[indexWithoutRange].qty);
-                        sellers[indexWithoutRange].qty -= buyers[indexWithRange].qty;
-                        buyers.RemoveAt(indexWithRange);
-                    }
-                }
-                else if(withOutRange.qty ==withRange.qty)
-                {
-                    if (buyers[indexWithoutRange] == withOutRange)
-                    {
-                        Console.WriteLine("Tradded Between Buyer" + buyers[indexWithoutRange].qty + " & seller" + sellers[indexWithRange].qty);
-                        buyers.RemoveAt(indexWithoutRange);
-                        sellers.RemoveAt(indexWithRange);
-                    }
-                    else
-                    {
-                        Console.WriteLine("Tradded Between Buyer" + buyers[indexWithRange].qty + " & seller" + sellers[indexWithoutRange].qty);
-                        buyers.RemoveAt(indexWithRange);
-                        sellers.RemoveAt(indexWithoutRange);
-                    }
-                }
-                else
-                {
-                    if (buyers[indexWithoutRange] == withOutRange)
-                    {
-                        Console.WriteLine("Tradded Between Buyer" + buyers[indexWithoutRange].qty + " & seller" + sellers[indexWithRange].qty);
-                        sellers[indexWithRange].qty-=buyers[indexWithoutRange].qty;
-                        buyers.RemoveAt(indexWithoutRange);
-                    }
-                    else
-                    {
-                        Console.WriteLine("Tradded Between Buyer" + buyers[indexWithRange].qty + " & seller" + sellers[indexWithoutRange].qty);
-                        buyers[indexWithRange].qty -= sellers[indexWithoutRange].qty;
-                        sellers.RemoveAt(indexWithoutRange);
-                    }
-                }
-            }
-            else
-            {
-                flag = -1;
-            }
-            return flag;
         }
         public void solve()
         {
-            int i = 0,j=0,flag=0;
-
-            while( i < buyers.Count)
+            loopThrough(sellers, buyers);
+            clean(buyers,0);
+            clean(sellers,0);
+            loopThrough(buyers, sellers);
+            clean(buyers, 0);
+            clean(sellers, 0);
+            Console.WriteLine(" Reamining Sellers ");
+            for(int i = 0; i < buyers.Count; i++)
             {
-                if (buyers.Count == 0 || sellers.Count == 0 || j>=sellers.Count)
-                {
-                    break;
-                }
-
-                while (j < sellers.Count)
-                {
-                    if (buyers.Count == 0 || sellers.Count == 0)
-                    {
-                        break;
-                    }
-                    //No Condtition at Buyer,No COndition At Seller
-                    if (buyers[i].a == 0 && sellers[j].a == 0 && buyers[i].range == 0 && sellers[j].range == 0)
-                    {
-                        simplyUpdate(i, j);
-                        continue;
-                    }
-
-                    //No condition at buyer,All Or None At Seller
-                    if (buyers[i].a == 0 && buyers[i].range == 0 && sellers[j].a != 0 && sellers[j].range == 0)
-                    {
-                        flag = withOneALlOrNoneCondition(buyers[i], sellers[j], i, j);
-                        if (flag == -1)
-                        {
-                            j++;
-                        }
-                    }
-
-                    //All or None At buyer,No condtition at seller
-                    if (buyers[i].a != 0 && buyers[i].range == 0 && sellers[j].a == 0 && sellers[j].range == 0)
-                    {
-                        flag = withOneALlOrNoneCondition(sellers[j],buyers[i], j, i);
-                        if (flag == -1)
-                        {
-                            i++;
-                        }
-                    }
-
-                    //No condition at buyer,Range at seller side
-                    if (buyers[i].a==0 && buyers[i].range == 0 && sellers[j].a == 0 && sellers[j].range != 0)
-                    {
-                        flag = withOneRangeCondition(buyers[i], sellers[j], i, j);
-                        if (flag == -1)
-                        {
-                            j++;
-                        }
-                    }
-
-                    //Range at Buyer,No condition at seller 
-                    if (buyers[i].a == 0 && buyers[i].range != 0 && sellers[j].a == 0 && sellers[j].range == 0)
-                    {
-                        flag = withOneRangeCondition(sellers[j], buyers[i], j,i);
-                        if (flag == -1)
-                        {
-                            i++;
-                        }
-                    }
-                }
+                Console.WriteLine(buyers[i].toString());
+            }
+            Console.WriteLine(" Reamining Buyers ");
+            for (int i = 0; i < sellers.Count; i++)
+            {
+                Console.WriteLine(sellers[i].toString());
             }
         }
         static void Main(string[] args)
@@ -215,8 +244,6 @@ namespace MatchingPairAlgorithm_BuyerSelller
             int a = 0;
             int r = 0;
             Demo d = new Demo();
-
-            
             Console.WriteLine("Enter -999 to Stop");
             Console.WriteLine("Buyers:");
             while (input!=-999)
@@ -227,7 +254,7 @@ namespace MatchingPairAlgorithm_BuyerSelller
                 a = Int32.Parse(Console.ReadLine());
                 Console.WriteLine("Enter Ticket Size");
                 r = Int32.Parse(Console.ReadLine());
-                d.buyers.Add(new party(qty, a, r));
+                d.buyers.Add(new NewParty(qty, a, r));
                 Console.WriteLine("Enter -999 to Stop,anyother number to continue");
                 input = Int32.Parse(Console.ReadLine());
             }
@@ -241,7 +268,7 @@ namespace MatchingPairAlgorithm_BuyerSelller
                 a = Int32.Parse(Console.ReadLine());
                 Console.WriteLine("Enter Ticket Size");
                 r = Int32.Parse(Console.ReadLine());
-                d.sellers.Add(new party(qty, a, r));
+                d.sellers.Add(new NewParty(qty, a, r));
                 Console.WriteLine("Enter -999 to Stop,anyother number to continue");
                 input = Int32.Parse(Console.ReadLine());
             }
---
+++ MatchingPairWithParameteres.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+
+public class Order
+{
+    public int quantity;
+    public int ticketSize;
+    public int allOrNone;
+    public int partyNo;
+    public static int partyID = 0;
+    public boolean isVoid;
+    public Order(int q,int ticket,int all)
+    {
+        this.quantity = q;
+        this.ticketSize = ticket;
+        this.allOrNone = all;
+        this.partyNo = partyID;
+        Order.partyID++;
+        this.isVoid = false;
+    }
+    public void makeVoid()
+    {
+        this.isVoid = true;
+    }
+}
+public class MatchingPairAlog
+{
+    public void doTrade(Order buyerParty,Order sellerParty,int quantity)
+    {
+        //Console.WriteLine//
+    }
+    public void executeOrdersRecursive(int startIndex,int windowSize,ArrayList<Order> sellerList,)
+    {
+
+    }
+    public void  executeOrders(ArrayList<Order> sellerList,ArrayList<Order> buyerList,int sellerIndex,int buyerIndex)
+    {
+        if (IsCriteriaFullfilling(sellerList.ToArray()[sellerIndex], buyerList.ToArray[buyerIndex]))
+        {
+            int quantity = getQuantity(sellerList.ToArray()[sellerIndex], buyerList.ToArray[buyerIndex]);
+            doTrade(sellerList.ToArray()[sellerIndex], buyerList.ToArray[buyerIndex], quantity);
+            updateArraylist(buyerList, buyerList.ToArray()[buyerIndex],quantity);
+            updateArraylist(sellerList, sellerList.ToArray()[sellerIndex],quantity);
+            executeOrders(sellerList, buyerList, 0, buyerIndex + 1);
+        }
+        else
+        {
+            int startBuyer = 0;
+            int endBuyer = buyerIndex;
+            int windowSize = 2;
+            while(windowSize<=(end)
+        }
+    }
+    public boolean isFullfillingCriteria(Party p1,Party p2)
+    {
+        if (p1.price == p2.price)
+            return true;
+        else
+        {
+            if (p1.a && p2.a)
+            {
+                return false;
+            }
+            else if (p1.a && p2.range)
+        }
+    }
+    public void Program(Queue<Party> BidderQueue, Queue<Party> OfferQueue)
+    {
+            
+    }
+}
